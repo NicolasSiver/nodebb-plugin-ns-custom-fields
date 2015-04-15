216d46824fb25886be2fdfd1cf1740f41829a8cc
Nicolas Siver <nicolas@siver.im>
2015-04-16 00:03:49 +0200

    Fix statics

--- library.js
+++ library.js
@@ -6,39 +6,39 @@ var _         = require('underscore'),
     meta      = module.parent.require('./meta'),
 
     settings  = null,
-    namespace = 'ns:custom-fields';
+    namespace = 'ns:custom_fields';
 
 (function (Plugin) {
     Plugin.hooks = {
         filters: {
-            menu   : function (custom_header, callback) {
+            menu: function (custom_header, callback) {
                 custom_header.plugins.push({
                     route: '/plugins/custom-fields',
                     icon : 'fa-plus-square',
                     name : 'Custom Fields'
                 });
                 callback(null, custom_header);
-            },
-            statics: {
-                load: function (params, callback) {
-                    var router      = params.router,
-                        middleware  = params.middleware,
-                        controllers = params.controllers;
+            }
+        },
+        statics: {
+            load: function (params, callback) {
+                var router      = params.router,
+                    middleware  = params.middleware,
+                    controllers = params.controllers;
 
-                    meta.settings.get(namespace, function (err, config) {
-                        if (err) {
-                            winston.warn('[' + namespace + '] Error has been occurred! There is no configuration.');
-                            return callback(err);
-                        }
+                meta.settings.get(namespace, function (err, config) {
+                    if (err) {
+                        winston.warn('[' + namespace + '] Error has been occurred! There is no configuration.');
+                        return callback(err);
+                    }
 
-                        settings = config;
+                    settings = config;
 
-                        router.get('/admin/plugins/custom-fields', middleware.applyCSRF, middleware.admin.buildHeader, Plugin.renderAdmin);
-                        router.get('/api/admin/plugins/custom-fields', middleware.applyCSRF, Plugin.renderAdmin);
+                    router.get('/admin/plugins/custom-fields', middleware.applyCSRF, middleware.admin.buildHeader, Plugin.renderAdmin);
+                    router.get('/api/admin/plugins/custom-fields', middleware.applyCSRF, Plugin.renderAdmin);
 
-                        callback();
-                    });
-                }
+                    callback();
+                });
             }
         }
     };

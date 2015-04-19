7a2e99c5d31962fd44746e41585dfab945c0def0
Nicolas Siver <nicolas@siver.im>
2015-04-19 09:52:03 +0200

    Db: create implementation

--- app/database.js
+++ app/database.js
@@ -1,10 +1,39 @@
 (function (Database) {
     'use strict';
 
-    var db = require('./nodebb').db;
+    var db        = require('./nodebb').db,
+        namespace = 'ns:custom_fields';
 
+    //TODO Use async waterfall
     Database.createField = function (key, name, done) {
+        db.incrObjectField('global', 'nextNsCustomFieldId', function (error, id) {
+            if (error) {
+                return done(error);
+            }
 
+            //Any incremental integer will suffice
+            var sortPosition = id;
+
+            db.sortedSetAdd(namespace, sortPosition, id, function (error) {
+                if (error) {
+                    return done(error);
+                }
+
+                var fieldModel = {
+                    fid : id,
+                    key : key,
+                    name: name
+                };
+
+                db.setObject(namespace + ':' + id, fieldModel, function (error) {
+                    if (error) {
+                        return done(error);
+                    }
+
+                    done(null, fieldModel);
+                });
+            });
+        });
     };
 
     Database.getFields = function (done) {
